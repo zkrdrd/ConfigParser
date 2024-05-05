89f52068cf2e1aa16d671d4350fcbbbcdf93b99f
dev_delphin <eduard.zakirov.96@mail.ru>
2024-05-05 07:56:33 +0500

    tests

--- parser_test.go
+++ parser_test.go
@@ -14,38 +14,44 @@ type HostParser struct {
 
 func TestRead(t *testing.T) {
 	t.Parallel()
-	t.Run(`test find min number`, func(t *testing.T) {
+	t.Run(`test pase file`, func(t *testing.T) {
 		testTable := []struct {
-			Values string
-			Result string
+			Values      string
+			ErrorResult string
+			HostResult  string
+			PortResult  int
 		}{
 			{
-				Values: "configs/config.json",
-				Result: "&{1.1.1.1 123}",
+				Values:     "configs/config.json",
+				HostResult: "1.1.1.1",
+				PortResult: 123,
 			},
 			{
-				Values: "configs/config.yaml",
-				Result: "&{2.2.2.2 456}",
+				Values:     "configs/config.yaml",
+				HostResult: "2.2.2.2",
+				PortResult: 456,
 			},
 			{
-				Values: "configs/config",
-				Result: "&{1.1.1.1 123}",
+				Values:     "configs/config",
+				HostResult: "1.1.1.1",
+				PortResult: 123,
 			},
 			{
-				Values: "configs/config2",
-				Result: "&{2.2.2.2 456}",
+				Values:     "configs/config2",
+				HostResult: "2.2.2.2",
+				PortResult: 456,
 			},
 			{
-				Values: "configs/noFile",
-				Result: "open configs/noFile: no such file or directory",
+				Values:      "configs/noFile",
+				ErrorResult: "open configs/noFile: no such file or directory",
 			},
 			{
-				Values: "configs/empty",
-				Result: "file is empty",
+				Values:      "configs/empty",
+				ErrorResult: "file is empty",
 			},
 			{
-				Values: "configs/noFormat.txt",
-				Result: "parser not found",
+				Values:      "configs/noFormat.txt",
+				ErrorResult: "parser not found",
 			},
 		}
 
@@ -58,16 +64,18 @@ func TestRead(t *testing.T) {
 			go func() {
 				defer wg.Done()
 
-				// Проверяем поиск наименьшего числа
 				var cfg = &HostParser{}
-				c := parser.Read(expect.Values, cfg)
-				if c != nil {
-					if expect.Result != fmt.Sprintf("%v", c) {
-						t.Error(fmt.Errorf(`result %v != %v`, expect.Result, c))
+				resultParse := parser.Read(expect.Values, cfg)
+				if resultParse != nil {
+					if expect.ErrorResult != fmt.Sprintf("%v", resultParse) {
+						t.Error(fmt.Errorf(`result %v != %v`, expect.HostResult, resultParse))
 					}
 				} else {
-					if expect.Result != fmt.Sprintf("%v", cfg) {
-						t.Error(fmt.Errorf(`result %v != %v`, expect.Result, cfg))
+					if expect.HostResult != cfg.Host {
+						t.Error(fmt.Errorf(`result %v != %v`, expect.HostResult, cfg.Host))
+					}
+					if expect.PortResult != cfg.Port {
+						t.Error(fmt.Errorf(`result %v != %v`, expect.PortResult, cfg.Port))
 					}
 				}
 			}()
